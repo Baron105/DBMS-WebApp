e61d3435272f71b6bc7106882a24a634b51222f8
Baron105 <barunparua@gmail.com>
2024-02-27 14:22:27 +0530

    od

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def home():
     """Home page"""
     return render_template('home.html')
 
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    """Login page"""
+    return render_template('login.html')
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    """Register page"""
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
