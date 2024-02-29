e50dc4d46b55bafb6e221412d6fa85b63f9ac82a
Warpidee <85791415+Warpidee@users.noreply.github.com>
2024-02-29 18:28:51 +0530

    css changes

--- static/js/script.js
+++ static/js/script.js
@@ -19,3 +19,43 @@ function changeReadMore(eventId) {
         eventTiming.style.display = 'flex';
     }
 }
+
+var et = document.querySelector('#my_variable').value
+
+    function toggleDiv1() {
+        var div = document.getElementById('div1');
+        var div2 = document.getElementById('div2');
+        var div3 = document.getElementById('div3');
+        div.style.display = 'block';
+        div2.style.display = 'none';
+        div3.style.display = 'none';
+        
+    }
+    function toggleDiv2() {
+        var div = document.getElementById('div2');
+        var div1 = document.getElementById('div1');
+        var div3 = document.getElementById('div3');
+        div.style.display = 'block';
+        div1.style.display = 'none';
+        div3.style.display = 'none';
+
+    }
+    function toggleDiv3() {
+        var div = document.getElementById('div3');
+        var div1 = document.getElementById('div1');
+        var div2 = document.getElementById('div2');
+        div.style.display = 'block';
+        div1.style.display = 'none';
+        div2.style.display = 'none';
+    }
+    function load(et){
+        if(et == 1) {
+            toggleDiv2();
+        }
+        else if(et == 2) {
+            toggleDiv3();
+        }
+        else {
+            toggleDiv1();
+        }
+    }
