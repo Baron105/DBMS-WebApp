22056b259463f1c6defe50decfdc3f8264e8de08
Warpidee <85791415+Warpidee@users.noreply.github.com>
2024-03-01 02:22:59 +0530

    Dashboard Profile page complete

--- static/js/script.js
+++ static/js/script.js
@@ -20,68 +20,55 @@ function changeReadMore(eventId) {
     }
 }
 
-var et = document.querySelector('#my_variable').value
+function toggleDiv1() {
+    var div = document.getElementById('profile-tab');
+    var div2 = document.getElementById('organize-tab');
+    var div3 = document.getElementById('edit-tab');
+    var profile = document.getElementById('pf-content');
+    var organize = document.getElementById('organize-content');
+    var edit = document.getElementById('edit-content');
+    div.style.backgroundColor = "#F2EFF3";
+    div.style.color = "#4F708D"
+    profile.style.display = "flex";
+    div2.style.backgroundColor = "inherit";
+    div2.style.color = "inherit";
+    organize.style.display = "none";
+    div3.style.backgroundColor = "inherit";
+    div3.style.color = "inherit";
+    edit.style.display = "none";
+}
+function toggleDiv2() {
+    var div = document.getElementById('profile-tab');
+    var div2 = document.getElementById('organize-tab');
+    var div3 = document.getElementById('edit-tab');
+    var profile = document.getElementById('pf-content');
+    var organize = document.getElementById('organize-content');
+    var edit = document.getElementById('edit-content');
+    div2.style.backgroundColor = "#F2EFF3";
+    div2.style.color = "#4F708D";
+    organize.style.display = "inherit";
+    div.style.backgroundColor = "inherit";
+    div.style.color = "inherit";
+    profile.style.display = "none";
+    div3.style.backgroundColor = "inherit";
+    div3.style.color = "inherit";
+    edit.style.display = "none";
 
-    function toggleDiv1() {
-        var div = document.getElementById('profile-tab');
-        var div2 = document.getElementById('organize-tab');
-        var div3 = document.getElementById('edit-tab');
-        var profile = document.getElementById('profile-content');
-        var organize = document.getElementById('organize-content');
-        var edit = document.getElementById('edit-content');
-        div.style.backgroundColor = "#F2EFF3";
-        div.style.color = "#4F708D"
-        profile.style.display = "inherit";
-        div2.style.backgroundColor = "inherit";
-        div2.style.color = "inherit";
-        organize.style.display = "none";
-        div3.style.backgroundColor = "inherit";
-        div3.style.color = "inherit";
-        edit.style.display = "none";
-    }
-    function toggleDiv2() {
-        var div = document.getElementById('profile-tab');
-        var div2 = document.getElementById('organize-tab');
-        var div3 = document.getElementById('edit-tab');
-        var profile = document.getElementById('profile-content');
-        var organize = document.getElementById('organize-content');
-        var edit = document.getElementById('edit-content');
-        div2.style.backgroundColor = "#F2EFF3";
-        div2.style.color = "#4F708D";
-        organize.style.display = "inherit";
-        div.style.backgroundColor = "inherit";
-        div.style.color = "inherit";
-        profile.style.display = "none";
-        div3.style.backgroundColor = "inherit";
-        div3.style.color = "inherit";
-        edit.style.display = "none";
-
-    }
-    function toggleDiv3() {
-        var div = document.getElementById('profile-tab');
-        var div2 = document.getElementById('organize-tab');
-        var div3 = document.getElementById('edit-tab');
-        var profile = document.getElementById('profile-content');
-        var organize = document.getElementById('organize-content');
-        var edit = document.getElementById('edit-content');
-        div3.style.backgroundColor = "#F2EFF3";
-        div3.style.color = "#4F708D";
-        edit.style.display = "inherit";
-        div.style.backgroundColor = "inherit";
-        div.style.color = "inherit";
-        profile.style.display = "none";
-        div2.style.backgroundColor = "inherit";
-        div2.style.color = "inherit";
-        organize.style.display = "none";
-    }
-    function load(et){
-        if(et == 1) {
-            toggleDiv2();
-        }
-        else if(et == 2) {
-            toggleDiv3();
-        }
-        else {
-            toggleDiv1();
-        }
-    }
+}
+function toggleDiv3() {
+    var div = document.getElementById('profile-tab');
+    var div2 = document.getElementById('organize-tab');
+    var div3 = document.getElementById('edit-tab');
+    var profile = document.getElementById('pf-content');
+    var organize = document.getElementById('organize-content');
+    var edit = document.getElementById('edit-content');
+    div3.style.backgroundColor = "#F2EFF3";
+    div3.style.color = "#4F708D";
+    edit.style.display = "inherit";
+    div.style.backgroundColor = "inherit";
+    div.style.color = "inherit";
+    profile.style.display = "none";
+    div2.style.backgroundColor = "inherit";
+    div2.style.color = "inherit";
+    organize.style.display = "none";
+}
